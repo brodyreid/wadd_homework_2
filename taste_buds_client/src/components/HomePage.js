c0a85ea8b36c1e48459ecf8f4731c4e5ef0f0f42
brodyreid <brodyhreid@gmail.com>
2021-09-11 14:02:22 -0400

    done!

--- taste_buds_client/src/components/HomePage.js
+++ taste_buds_client/src/components/HomePage.js
@@ -4,6 +4,7 @@ class HomePage extends Component {
     render() {
         return(
             <div className="image w-100">
+                <h1 className="text-center">Welcome the Taste Buds</h1>
                 <img className="w-100 img-fluid" src="https://media.istockphoto.com/photos/stirring-some-love-into-it-picture-id1164904650?k=20&m=1164904650&s=612x612&w=0&h=mCPWCgFLCLp-d_LVmMRQSFGuToQjhE9sZoxe8Gh9mhc="/>
             </div>
         )
